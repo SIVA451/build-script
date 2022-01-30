3f2decd96fd2f3efcb6a8d0643a9ec9e82d99bc9
sivasankar.nagireddy <13656990+SIVA451@users.noreply.github.com>
2022-01-30 23:00:52 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
+yum update -y
+yum install -y httpd
+systemctl start httpd.service
+systemctl enable httpd.service
 cd /var/www/html;
 git clone https://github.com/SIVA451/Sample-Build.git;
 systemctl restart httpd.service
